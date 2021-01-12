18f60c90d500af5cb81ec53a8b380a188c5e301b
Pavel Vasev <pavel.vasev@gmail.com>
2021-01-12 17:25:33 +0500

    sounds

---
+++ points-game/sounds/files/LICENSE.md
@@ -0,0 +1,3 @@
+All these files are taken from https://freesound.org/ 
+and belongs to respective owners under their licenses.
+Names of files are kept intact.
--- points-game/sounds/init.js
+++ points-game/sounds/init.js
@@ -1,94 +1,107 @@
 /////////////// feature
 
-var presets = [
-  "none",
-  "custom",
-  "324612__gokhanbiyik__arp-02.wav",
-  "414619__pjcohen__maraca-low-shake-velocity-11.wav",
-  "30341__junggle__waterdrop24.wav",
-  "43677__stijn__click11.wav",
-  "269583__ifrosta__bubble.wav"
-];
+var dir = import.meta.url.substr( 0,import.meta.url.lastIndexOf("/") ) + "/files/";
+
+var presets = {
+  "none":null,
+  "custom":null,
+  "arp":dir+"324612__gokhanbiyik__arp-02.wav",
+  "maracas":dir+"414619__pjcohen__maraca-low-shake-velocity-11.wav",
+  "waterdrop":dir+"30341__junggle__waterdrop24.wav",
+  "click":dir+"43677__stijn__click11.wav",
+  "bubble":dir+"269583__ifrosta__bubble.wav",
+  "baraban":dir+"27359__junggle__djembe-loop-13.wav",
+  "kick":dir+"131770__bertrof__sub-kick-long.wav",
+  "piano":dir+"32158__zin__piano-2-140bpm.wav",
+  "guitar":dir+"181425__serylis__guitar-chord.wav",
+  "tababa-bamm":dir+"4852__zajo__loop02.wav"
+};
 
 /// todo: add files hide/show if custom option is selected
 
 export function setup( vz, game ) {
   var obj = game.ns.getChildByName( "sounds" ) || game.create_obj( {}, {name:"sounds"} );
-  
-  var dir = import.meta.url.substr( 0,import.meta.url.lastIndexOf("/") ) + "/files";
-  var active=true;
-  
-//  obj.addSlider( "active",1,0,1,1,function(v) { active = v; } );
 
   obj.addSlider( "volume",0.5, 0,1,0.05, function() {} );
   
-  obj.addCombo( "appear-sound-preset",0,presets,function(v) {
-    if (v > 1) {
-      var url = dir + "/" + presets[v];
-      obj.setParam("appear-sound",url );
-    }
-    else
-    if (v == 0)
-      obj.setParam("appear-sound","" );
-  });
-//  obj.addFile( "appear-sound","", function() {} );  
-
-  obj.addCombo( "hit-sound-preset",2,presets,function(v) {
-    if (v > 1) {
-      var url = dir + "/" + presets[v];
-      obj.setParam("hit-sound",url );
-    }
-    else
-    if (v == 0)
-      obj.setParam("hit-sound","" );
-  });
+  function addsnd( name ) {
   
-//  obj.addFile( "hit-sound","", function() {} );
+     obj.addCombo( name+"-preset",0,Object.keys(presets),function(v) {
+      if (v > 1) {
+        var url = Object.values(presets)[v];
+        obj.setParam(name,url );
+      }
+      else
+      if (v == 0)
+        obj.setParam(name,"" );
+      obj.setGuiVisible( name,v == 1);
+    });
+    
+    obj.addFile( name,"", function(v) {
+      if (v instanceof File) {
+        obj.setParam( name, URL.createObjectURL( v ) );
+      }
+    } );
+    
+    obj.setGuiVisible( name, false );
+    
+    obj.addSlider( name+"-volume",1, 0,1,0.05, function() {} );
+  }
   
-  obj.addCombo( "boom-sound-preset",3,presets,function(v) {
-    if (v > 1) {
-      var url = dir + "/" + presets[v];
-      obj.setParam("boom-sound",url );
-    }
-      else 
-    if (v == 0)
-      obj.setParam("boom-sound","" );
-  });
-//  obj.addFile( "boom-sound","", function() {} );
+  addsnd( "appear-sound" );
+  addsnd( "hit-sound" );
+  addsnd( "boom-sound" );
 
   obj.addCmd("Sounds at freesound.org",function() {
      window.open("https://freesound.org/");
   });
 
   ///////////////////////////////////////////////////////
+  
+  function playsnd( name,index ) {
+    var volume = obj.getParam("volume");
+    var thisvolume = obj.getParam(name+"-volume");
+    volume = volume * thisvolume;
+    if (volume <= 0) return;
+    var pos = game.getPos( index );
+    var url = obj.getParam( name );
+    vz.vis.playSound3d( pos,url,function(sound) {
+      //if (name == "appear") volume = volume * 0.5;
+      sound.setVolume( volume );
+    } );
+  }
 
+  
+  var instep = {};
+  game.chain("step", function() {
+    instep = {};
+    this.orig.apply( game, arguments );
+  });
+  function goif( name,limit,fn ) {
+    instep[name] = (instep[name] || 0) +1;
+    if (instep[name] > limit && Math.random() > 0.05) return;
+    fn();
+  }
+  
   game.chain("setState", function(index, value) {
-//    if (active) {
-
+    var prev = game.getState(index);
     if (value == 1) { // appear
-      var pos = game.getPos( index );
-      var url = obj.getParam("appear-sound"); 
-      vz.vis.playSound3d( pos,url,function(sound) {
-        sound.setVolume( obj.getParam("volume") );
-      } );
+      goif( "appear",10, function() {
+        playsnd( "appear-sound", index );
+      });
     }
     else
-    if (value == 254) { // boom
-      var pos = game.getPos( index );
-      var url = obj.getParam("boom-sound"); 
-      vz.vis.playSound3d( pos,url,function(sound) {
-        sound.setVolume( obj.getParam("volume") );
-      } );
+    if (value == 254 && prev != value) { // boom
+      goif( "boom",10, function() {
+        playsnd( "boom-sound", index );
+      });
     }
     else
-    if (value == 255) { // heal
-      var pos = game.getPos( index );
-      var url = obj.getParam("hit-sound"); 
-      vz.vis.playSound3d( pos,url,function(sound) {
-        sound.setVolume( obj.getParam("volume") );
-      } );    
+    if (value == 255 && prev != value) { // heal
+      goif( "hit",1, function() {
+        playsnd( "hit-sound", index );
+      });
     }
-//    }
     this.orig( index, value );
   });
 
