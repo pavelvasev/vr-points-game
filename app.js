2aa2e71aeabd66dbc95823b60ec1c0d31b871223
Pavel Vasev <pavel.vasev@gmail.com>
2021-01-29 21:32:40 +0500

    better

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 import krako_setup from "./krako.js";
 import points_game_setup from "./points-game/init.js";
 
-krako_setup( vz );
-points_game_setup( vz );
+// place your app code in this function
+export function create( vz, opts ) {
+  krako_setup( vz );
+  points_game_setup( vz );
+  
+  var game = vz.create_obj_by_type( {type: "points-game", name: "points-game"} );
+  debugger;
+  var krako = vz.create_obj_by_type( {type: "krakozabra",manual:true, parent: game} );
+  game.setParam( "points", vz.get_path( krako.ns.getChildren()[0] ) );
 
-var krako = vz.create_obj_by_type( {type: "krakozabra",manual:true } );
-var game = vz.create_obj_by_type( {type: "points-game"} );
-game.setParam( "points", vz.get_path( krako.ns.getChildren()[0] ) );
-
-vz.restoreFromHash();
-
-game.restart();
+  game.restart();
+  
+  return game;
+}
